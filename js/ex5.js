002366c16b6e94e1cc684f2ad8a56d10c609ccd7
Arham Shoaib <arhamshoaib22@outlook.com>
2022-03-10 19:44:07 -0800

    Third Commit

--- js/ex5.js
+++ js/ex5.js
@@ -4,8 +4,13 @@ let tbody = document.createElement('tbody');
 table.appendChild(tbody);
 document.getElementById("body").appendChild(table);
 
-let row_1 = document.createElement('tr');
-let row_1_data_1 = document.createElement('td');
-row_1_data_1.innerHTML = "1";
-row_1.appendChild(row_1_data_1);
-tbody.appendChild(row_1);
+for ( var i=1; i < 13; i++) {
+    let row_1 = document.createElement('tr');
+    let row_1_data_1 = document.createElement('td');
+    let x = row_1_data_1.innerHTML = i;
+    row_1.appendChild(row_1_data_1);
+    tbody.appendChild(row_1);
+    if ( i % 4 == 0){
+        row_1_data_1.style.backgroundColor = " lightblue"
+    }
+}
